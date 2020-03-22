da0857ef0d5003674c59ce6de7d9845b9d526e02
KnownAsNob <theniallobrien@gmail.com>
2020-03-22 02:20:15 +0000

    Added cors whitelist

--- VisualizeApp/static/VisualizeApp/mapDisplayLogic.js
+++ VisualizeApp/static/VisualizeApp/mapDisplayLogic.js
@@ -114,8 +114,8 @@ function fetchData(type)
 		return $.ajax({
 			contentType: "application/x-www-form-urlencoded;charset=UTF-8",
 			type: "POST",
-			//beforeSend: function(xhr){xhr.setRequestHeader('X-CSRFToken', "{{csrf_token}}");},
-			url: decodeURI('{% url get-calls-year %}'),
+			beforeSend: function(xhr){xhr.setRequestHeader('X-CSRFToken', "{{csrf_token}}");},
+			url: "map/get_calls_year/",
 			datatype: "json",
 			//async: true,
 			data: {"type": type},
